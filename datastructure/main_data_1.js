74a4bac4d0051be223401bc49e1b3090b7feb753
evanchuamg <66861359+evanchuamg@users.noreply.github.com>
2024-02-26 23:43:43 +0800

    Update main_data_1.js
    Added linked list data structure

--- datastructure/main_data_1.js
+++ datastructure/main_data_1.js
@@ -1,19 +1,6 @@
 
 let ds = null // new.
 
-// ****** SHOW INSERT IDX INPUT ****** //
-const insert_idx_show = () => {
-    for (i=0;i<ds.total_ops+1;i++){
-        let operation_indiv_div = document.getElementById("operation_indiv_div"+i);
-        let value = operation_indiv_div.querySelector("#ops").value;
-        let idx_display = operation_indiv_div.querySelector("#idx_input");
-        if (value === "insert"){idx_display.style.display = "inline-block";}
-        else {idx_display.style.display = "none";}
-    }
-}
-document.getElementById("operation_div").addEventListener("change", insert_idx_show);
-// ****** SHOW INSERT IDX INPUT ****** //
-
 
 // ****** RESTARTING ENTIRE PAGE ****** //
 const restart_all = () => {
@@ -29,6 +16,8 @@ const restart_all = () => {
     document.getElementById("operation_div").style.display = "none";
     document.getElementById("show").style.display = "inline-block";
     document.getElementById("restart_ds").style.display = "none";
+    document.getElementById("input_list").disabled = false;
+    document.getElementById("data").disabled = false;
 }
 
 const restart_operation = () => {
@@ -46,7 +35,14 @@ const restart_operation = () => {
     document.getElementById("add_operation_button").style.display = "inline-block";
     
     const parent_div = document.getElementById("add_operation"); // remove all added operation
-    while (ds.total_ops>0){
+    document.getElementById("operation_indiv_div0").querySelector("#ops").style.background="white";
+    document.getElementById("build_button").style.background="white";
+
+    document.getElementById("build_button").disabled = false;
+    document.getElementById("operation_indiv_div0").querySelector("#ops").disabled = false;
+    document.getElementById("operation_indiv_div0").querySelector("#ops_input").disabled = false;
+
+    while (ds.total_ops>0){ // remove explanation
         child_div = parent_div.querySelector("#operation_indiv_div"+ds.total_ops);
         parent_div.removeChild(child_div);
         ds.total_ops--;
@@ -65,7 +61,6 @@ const restart_board = () => { // clear the webpage in animations
 
 const remove_input = () => {
     document.getElementById("ops_input").value = "";
-    document.getElementById("idx_input").value = "";
     // remove input of add operation
 }
 // ****** RESTARTING ENTIRE PAGE ****** //
@@ -79,6 +74,13 @@ const create_div = (ds,id,i) => {
     div.id = id+"_"+i;
     div.textContent = id;
     div.style.left = (1+i)*100 + "px";
+    if (ds.data_structure==="linked_list"){ // creating arrow for linked list
+        if(i != ds.ani_list[ds.animate_stage][0].length-1){ // remove last arrow
+        div_arrow = document.createElement("arrow");
+        div.appendChild(div_arrow);
+        div_arrow.classList.add("arrow");
+        }
+    }
 }
 // ****** CREATING DIVS ****** //
 
@@ -91,7 +93,6 @@ const add_operation = () => {
     let add_operation = document.getElementById("add_operation");
     add_operation.appendChild(new_operation_indiv_div); // adding new ops as child div
     new_operation_indiv_div.id = "operation_indiv_div" + ds.total_ops;
-    add_operation.querySelector("#idx_input").style.display="none"; // bug that shows idx in add ops non insert
 }
 // ****** ADD OPERATIONS ****** //
 
@@ -99,17 +100,19 @@ const add_operation = () => {
 
 // ****** STARTING SIMULATION ****** //
 const start_data = () => {
-    let data = document.getElementById("data").value; // which Data structure
     let list = document.getElementById("input_list").value;
     const list_delimiter = list.split(",").map(s => Number(s));
     ds = new sorted_array(list_delimiter);
   
-    for (let i=0; i<ds.L.length; i++){
-      create_div(ds,ds.L[i],i) // show the unordered list
-    }
+// ****** showing what was input by user ****** //
+//    for (let i=0; i<ds.L.length; i++){
+//      create_div(ds,ds.L[i],i) // show the unordered list
+//    }
     document.getElementById("operation_div").style.display = "block";
     document.getElementById("show").style.display = "none";
     document.getElementById("restart_ds").style.display = "inline-block";
+    document.getElementById("input_list").disabled = true;
+    document.getElementById("data").disabled = true;
 }
 
 
@@ -122,7 +125,9 @@ const start_operation = (ds) => {
     document.getElementById("restart_ops_button").style.display = "inline-block";
     document.getElementById("target_value").style.display = "inline-block";
     document.getElementById("ani_value").style.display = "inline-block";
-    
+    document.getElementById("next_button").disabled = false;
+    document.getElementById("previous_button").disabled = true;
+
     // building data structure
     let data_structure = document.getElementById("data").value; // type of data structure
     if (data_structure === "sorted_array"){build_ds_sorted_arr(ds);}
@@ -131,31 +136,35 @@ const start_operation = (ds) => {
     ds.data_structure = data_structure;
 
     for (let i=0; i<ds.total_ops+1; i++){
-        console.log(i,ds.total_ops+1)
         ds.current_ops++
         execute_operation(i);
         ds.special = Array(ds.L.legnth).fill(0); // initialise ds.special
         console.log(ds.ani_list)
     }
+
     animate(ds);
 }
+
 
 const execute_operation = (i) => {
     let total_ops_name = "operation_indiv_div"+i;
     let total_ops = document.getElementById(total_ops_name);
     let ops = total_ops.querySelector("#ops").value;
     let input = Number(total_ops.querySelector("#ops_input").value); // target number
-    let insert_idx = total_ops.querySelector("#idx_input").value; // index for insert
-    if (ops === "find_linear"){find_x_linear(ds,input);} 
-    else if (ops === "find_binary"){find_x_binary(ds,input);}
-    else if (ops === "find_min"){find_min(ds,input);}
-    else if (ops === "find_max"){find_max(ds,input);}
-    else if (ops === "delete"){delete_x(ds,input);}
-    else if (ops === "insert"){insert_x(ds,input,insert_idx);}
     
+    if (ops === "find" && ds.data_structure === "sorted_array"){find_x_binary(ds,input);} 
+    else if (ops === "find" && ds.data_structure === "linked_list"){find_x_linear(ds,input);}
+    else if (ops === "find_min"){find_min(ds,input);}
+    else if (ops === "delete"){delete_x(ds,input);}
+    else if (ops === "insert"){insert_x(ds,input);} // remove insert idx
+    
+    // disable inputs
+    document.getElementById("build_button").disabled = true;
+    total_ops.querySelector("#ops").disabled = true;
+    total_ops.querySelector("#ops_input").disabled = true;
 }
 
-const hide_explain = (ops) => {
+const hide_explain = () => {
     let divs = document.querySelectorAll('[id$="_code"');
     divs.forEach(function(div){div.style.display = "none";}) // hides all the div
 }
@@ -184,11 +193,13 @@ const animate = (ds) => {
         }
     }
     
-    for (a=0;a<ds.total_ops+1;a++){
+    for (a=0;a<ds.total_ops+1;a++){ // for pseudocode animation
+        console.log(a,ds.total_ops+1)
         now_ops = document.getElementById("operation_indiv_div"+a).querySelector("#ops");
         if(current_ops === -1){ // for build function
             document.getElementById("build_button").style.background="lightgreen";
             document.getElementById("build_code").style.display = "block";
+            now_ops.style.background = "white";
         }
         else{
             document.getElementById("build_button").style.background="white";
@@ -196,7 +207,9 @@ const animate = (ds) => {
                 now_ops.style.background = "lightgreen";
                 console.log(now_ops.value);
                 hide_explain();
-                document.getElementById(now_ops.value+"_code").style.display = "block"; // show only that div 
+                if (ds.data_structure === "sorted_array" && now_ops.value === "find"){document.getElementById("find_binary_code").style.display = "block";}
+                else if (ds.data_structure === "linked_list" && now_ops.value === "find"){document.getElementById("find_linear_code").style.display = "block";}
+                else {document.getElementById(now_ops.value+"_code").style.display = "block";} // show only that div 
             }
             else {now_ops.style.background = "white";}    
         }
@@ -219,40 +232,46 @@ const animate = (ds) => {
 
 const next_step = (ds) => {
     ds.animate_stage++;
-    if (ds.animate_stage >= ds.ani_list.length){
-        ds.animate_stage--;
-        //let answer = window.confirm("No more next step. \n\n restart?")
-        //if (answer){restart_operation();}
-    //else{ds.animate_stage--;} // return to same animate_stage
+    animate(ds);
+    if (ds.animate_stage == ds.ani_list.length-1){
+        document.getElementById("next_button").disabled = true;
+        document.getElementById("previous_button").disabled = false;
     }
-    else{animate(ds);}
-}
+    else{
+        document.getElementById("next_button").disabled = false;
+        document.getElementById("previous_button").disabled = false;
+    }
+}   
   
 const previous_step = (ds) => {
-    if (ds.animate_stage < 0){
-        ds.animate_stage++;
-        //let answer = window.confirm("No more previous step. \n\n restart?");
-        //if (answer){restart_operation();}
-    //else{ds.animate_stage++;} // return to same animate_stage
+    ds.animate_stage--;
+    animate(ds);
+    if (ds.animate_stage == 0){
+        document.getElementById("next_button").disabled = false;
+        document.getElementById("previous_button").disabled = true;
     }
-    else{ds.animate_stage--;animate(ds);}
+    else{
+        document.getElementById("next_button").disabled = false;
+        document.getElementById("previous_button").disabled = false;
+    }
 }
 // ****** ANIMATION ****** //
 
 
 // ****** METHODS/FUNCTIONS ****** //
 const build_ds_sorted_arr = (ds) => {
-    ds.ani_list.push([structuredClone(ds.L),NaN,NaN,NaN,ds.current_ops]); // ordered list
+    ds.ani_list.push([structuredClone(ds.L),NaN,NaN,NaN,ds.current_ops]); // unordered list
     ds.L = ds.L.sort(function(a,b){return a-b;}); 
     ds.ani_list.push([structuredClone(ds.L),NaN,NaN,NaN,ds.current_ops]); // ordered list
 }
 
 const build_ds_linked_list = (ds) => {
-    // put building linked list algo here
-//    ds.ani_list.push([ds.L,NaN,NaN,NaN]); // ordered list
-//    for (let i=0; i<ds.L.length; i++){
-//      create_div(ds,ds.L[i],i)
-//    }
+    //ds.ani_list.push([structuredClone(ds.L),NaN,NaN,NaN,ds.current_ops]); // ordered list
+    if (ds.L.length === 0){ds.ani_list.push([structuredClone(ds.L),NaN,NaN,NaN,ds.current_ops]);}
+    for (let i=0; i<ds.L.length;i++){
+        current_list = ds.L.slice(0,i);
+        ds.ani_list.push([current_list,NaN,NaN,NaN,ds.current_ops]);
+    }
 }
 
 const build_ds_hash_table = (ds) => {
@@ -261,6 +280,7 @@ const build_ds_hash_table = (ds) => {
 //    for (let i=0; i<ds.L.length; i++){
 //      create_div(ds,ds.L[i],i)
 //    }
+    
 }
 
 
@@ -278,6 +298,11 @@ const find_x_linear = (ds,x) => {
 }
 
 const find_x_binary = (ds,x) =>{
+    if (ds.data_structure==="linked_list"){
+        window.alert("Linked_list has no option for binary search\nLinear search will be executed")
+        find_x_linear(ds,x);
+        return;
+    }
     const input = x;
     let end = ds.L.length - 1;  
     let start = 0;
@@ -309,11 +334,6 @@ const find_min = (ds) => {
     find_x_linear(ds,min);
 }
 
-const find_max = (ds) => {
-    let max = ds.L[ds.L.length-1];
-    find_x_linear(ds,max);
-}
-
 const delete_x = (ds,x) => { // find elem, then delete
     const input = Number(x)
     ds.number = input;
@@ -335,24 +355,17 @@ const delete_x = (ds,x) => { // find elem, then delete
     }
 }
 
-const insert_x = (ds,x,idx) => {
+const insert_x = (ds,x) => {
     const input = Number(x);
     ds.number = input;
-
-    for (let i=idx; i<ds.L.length; i++){ds.special[i] = 1;}
-    ds.ani_list.push([structuredClone(ds.L),NaN,input,structuredClone(ds.special),ds.current_ops]) // shade right list
-
     let new_list = structuredClone(ds.L);
-    new_list.splice(idx,0,""); // empty div into array
-
-    ds.L.splice(idx,0,input); // final list in ds after insert
-    ds.special[idx] = 0; // shift grey background to right
-    ds.special.push(1);
-
+    new_list.splice(0,0,""); // empty div into array
+    ds.L.splice(0,0,input); // final list in ds after insert
     ds.ani_list.push([new_list,NaN,input,structuredClone(ds.special),ds.current_ops]); // build left side with empty div
-    ds.ani_list.push([structuredClone(ds.L),idx,input,NaN,ds.current_ops]);
+    ds.ani_list.push([structuredClone(ds.L),0,input,NaN,ds.current_ops]);
     if (ds.data_structure == "sorted_array"){build_ds_sorted_arr(ds);}
 }
+
 
 
 // ****** DATA STRUCTURES ****** //
@@ -364,12 +377,33 @@ function sorted_array(L) {
     this.number = false; // target number
     this.animate_stage = 0;
     this.special = []; // background colour binary search
+    this.total_ops = 0; // for explanation and which operation
+    this.current_ops = -1;
+}
+
+function linked_list(head = null) {
+    this.head = head;   
+    this.data_structure = "";
+    
+    this.L = L;
+    this.ani_list = []; // [list, pointer, target, special,current_ops]
+    this.ani_explain = [];
+    this.number = false; // target number
+    this.animate_stage = 0;
+    this.special = []; // background colour binary search
     this.total_ops = 0;
     this.current_ops = -1;
 }
 
+function linked_list_node(data){
+    this.data = data;
+    this.next = null;
+}
 
-// change this.number to compare all the things required
+// ****** DATA STRUCTURES ****** //
+
+
+// chagne this.number to compare all the things required
 
 
 
